bc7a35054254f18b3f3b3f9a5fb6e5179d551a57
chistokrovnij <fsknsocco@gmail.com>
2024-07-10 00:44:25 +0300

    bag fixed

--- ConsoleApp9/Article.cs
+++ ConsoleApp9/Article.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp8
 {
-    internal class Article
+    public class Article
     {
         public int Id { get; set; }
         public string Name { get; set; }
